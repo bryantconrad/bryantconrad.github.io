810f67f0f3d5344379721f615bc7694e74c83e05
bryantconrad <bryant.conradl@gmail.com>
2021-04-14 02:59:11 -0400

    add work sections

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,27 @@
 $(document).ready(function () {
 
+    $('#project-links').hide();
+
+    const animateCSS = (element, animation, prefix = 'animate__') =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    const node = document.querySelector(element);
+
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve('Animation ended');
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd, {once: true});
+  });
+
     //set up waypoints
-    var active = 'active-nav'
+    var active = 'active-nav';
 
     var waypoint = new Waypoint({
         element: $('#about'),
@@ -28,9 +48,11 @@ $(document).ready(function () {
             if (direction == 'up') {
                 $('.links').removeClass(active);
                 $('#link-about').addClass(active);
+                $('#project-links').hide(100);
             } else {
                 $('.links').removeClass(active);
                 $('#link-work').toggleClass(active);
+                $('#project-links').show(100);
             }
         },
         offset: '60%'
@@ -44,9 +66,11 @@ $(document).ready(function () {
             if (direction == 'up') {
                 $('.links').removeClass(active);
                 $('#link-work').addClass(active);
+                $('#project-links').show(100);
             } else {
-            }
-        },
+                $('#project-links').hide(100);
+        }
+    },
         offset: '60%'
     })
 
